414731c35e7d4522fc9f1e05a39da8610b5cab6f
johnsonzsong <johnsonzsong@gmail.com>
2025-07-28 22:48:10 -0400

    next deployment configs

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = { 
+    basePath: isProd ? '/johnsonzsong.github.io' : '',
+    assetPrefix: isProd ? '/johnsonzsong.github.io/' : '',
+    images: { unoptimized: true },
+    output: 'export',
+};
 
 module.exports = nextConfig
